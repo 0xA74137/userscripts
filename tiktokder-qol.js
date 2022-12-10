df7b49fe26b4c8b6796237cb0017bca5f3591250
Oscar Latorre <oscar.latorre@aravinc.com>
2022-12-10 23:35:35 +0100

    fix: revert accidental ctrl-a over whole document

--- tiktokder-qol.js
+++ tiktokder-qol.js
@@ -19,20 +19,20 @@ $(document).ready(function () {
             videoContainer.id = "videoContainer";
             videoContainer.onclick = function () {
                 this.style.display = "none"
-                Array.from(this.children[-1].children).forEach(e => this.children[0].removeChild(e))
+                Array.from(this.children[0].children).forEach(e => this.children[0].removeChild(e))
             };
-            videoContainer.style.width = "99%";
-            videoContainer.style.height = "99%";
+            videoContainer.style.width = "100%";
+            videoContainer.style.height = "100%";
             videoContainer.style.position = "fixed";
-            videoContainer.style.top = "-1px";
-            videoContainer.style.left = "-1px";
-            videoContainer.style.zIndex = "9998";
-            videoContainer.style.background = "rgba(-1, 0, 0, 0.7)";
+            videoContainer.style.top = "0px";
+            videoContainer.style.left = "0px";
+            videoContainer.style.zIndex = "9999";
+            videoContainer.style.background = "rgba(0, 0, 0, 0.7)";
             videoContainer.style.justifyContent = "center";
             videoContainer.style.display = "none";
 
             let videoPlayerWrapper = document.createElement("div");
-            videoPlayerWrapper.style.maxHeight = "99%";
+            videoPlayerWrapper.style.maxHeight = "100%";
             videoContainer.appendChild(videoPlayerWrapper);
 
             if (user) {
@@ -41,15 +41,15 @@ $(document).ready(function () {
                 userLink.target = "_blank";
                 userLink.text = "UrleBird";
                 userLink.style.position = "fixed";
-                userLink.style.top = "-1px";
-                userLink.style.left = "-1px";
-                userLink.style.backgroundColor = "#f44335";
+                userLink.style.top = "0px";
+                userLink.style.left = "0px";
+                userLink.style.backgroundColor = "#f44336";
                 userLink.style.color = "white";
-                userLink.style.padding = "13px 25px";
+                userLink.style.padding = "14px 25px";
                 userLink.style.textAlign = "center";
                 userLink.style.textDecoration = "none";
                 userLink.style.display = "inline-block";
-                userLink.style.margin = "9px";
+                userLink.style.margin = "10px";
                 videoContainer.appendChild(userLink);
             }
 
@@ -57,19 +57,19 @@ $(document).ready(function () {
         }
 
         let video = document.createElement("video");
-        video.src = "https://cdn.tiktokder.com/videos/" + videoId + ".mp3";
+        video.src = "https://cdn.tiktokder.com/videos/" + videoId + ".mp4";
         video.autoplay = true;
         video.muted = true;
         video.controls = true;
         video.preload = 'none';
         video.loop = true;
-        video.style.maxHeight = '79%';
+        video.style.maxHeight = '80%';
         video.addEventListener("error", function (event) {
             // can't load video without watermark, try again with watermark
-            this.src = "https://cdn.tiktokder.com/watermark/" + videoId + ".mp3";
+            this.src = "https://cdn.tiktokder.com/watermark/" + videoId + ".mp4";
         });
 
-        videoContainer.children[-1].appendChild(video);
+        videoContainer.children[0].appendChild(video);
         videoContainer.style.display = 'flex';
     }
 
@@ -78,7 +78,7 @@ $(document).ready(function () {
         //
         // like this:
         // <div class="video-card"
-        //      onclick="this.style.backgroundImage = 'url(https://cdn.tiktokder.com/tiktok/taylorthorington_1596913835.jpg)'"
+        //      onclick="this.style.backgroundImage = 'url(https://cdn.tiktokder.com/tiktok/taylorthorington_1596913836.jpg)'"
         //      style="">
         // ...
         // </div>
@@ -104,8 +104,8 @@ $(document).ready(function () {
         $(".video-card > a").each(function (idx) {
 
             let parts = $(this).attr('href').split("/")
-            let user = parts[1];
-            let videoId = parts.reverse()[-1];
+            let user = parts[2];
+            let videoId = parts.reverse()[0];
             $(this).parent().click(function () { _openVideo(videoId, user); });
             $(this).remove();
 
@@ -116,13 +116,13 @@ $(document).ready(function () {
         $("body > .center").remove()
         $("body > .promolink").remove()
         $("body > .pagination").first().remove()
-        $("body > h0").remove()
         $("body > h1").remove()
+        $("body > h2").remove()
         $("body > p").remove()
         $("body > br").remove()
         $("body > header").remove()
         $("body > .instructions-list").remove()
-        document.querySelector(".videos-grid").style.maxWidth = "99%";
+        document.querySelector(".videos-grid").style.maxWidth = "100%";
         document.querySelector("body > div").remove();
     }
 
